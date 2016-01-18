ebfae15335e539ac22d9c34918ca06a88b7da501
WangWeigao <wangweigao@gmail.com>
2016-01-18 15:20:14 +0800

    user login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,15 +14,16 @@ use App\Module;
 class UserController extends Controller
 {
 
-    public function __construct(){
-    $this->middleware('auth');
-    }
+    // public function __construct(){
+    // $this->middleware('auth');
+    // }
+    //
+    // public function profile(Request $request)
+    // {
+    //     $user = $request->user();
+    //     echo $user['name'].'登录成功！';
+    // }
 
-    public function profile(Request $request)
-    {
-        $user = $request->user();
-        echo $user['name'].'登录成功！';
-    }
 
     /**
      * 模糊查询用户信息
