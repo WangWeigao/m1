f1099a6c88e1142ff81acffa44ac7094b6ce216c
WangWeigao <wangweigao@gmail.com>
2016-01-29 09:58:08 +0800

    highlight the category of Home, 用户管理, 教师管理, 订单管理, 发布审批

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
     var url = window.location.pathname;
+    var patt = new RegExp('\d');
     switch (url) {
         case '/home':
             $("#home").addClass('active');
             break;
         case '/user':
+        case '/getusers':
             $("#user-manager").addClass('active');
             break;
         case '/teacher':
+        case '/getteachers':
             $("#teacher-manager").addClass('active');
             break;
         case '/order':
+        case '/getorders':
+        case '/orderdetail/':
             $("#order-manager").addClass('active');
             break;
-
+        case '/lesson':
+            $("#lesson-manager").addClass('active');
         default:
 
     }
