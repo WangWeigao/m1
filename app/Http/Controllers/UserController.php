09427edcbbfa92d3686c9a8dedb752f04a77bba5
WangWeigao <wangweigao@gmail.com>
2016-02-04 17:03:24 +0800

    实现按"用户名"和"手机号"搜索用户

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,13 +48,19 @@ class UserController extends Controller
         $order = $request->get('order');
 
         //模糊匹配, 查询结果为分页做准备
-        $users = StudentUser::where('usertype', 1)
-                     ->where('nickname', 'like', "%$name%")
-                     ->leftjoin('orders', 'users.uid', '=', 'orders.student_uid')
-                     ->select('users.uid', 'users.nickname', 'users.cellphone', 'users.email', 'users.regdate', 'users.lastlogin', 'users.isactive', DB::raw('count(orders.student_uid) as order_num'))
-                     ->groupby('users.uid')
-                     ->orderby($field, $order)
-                     ->get();
+        $users = StudentUser::where('usertype', 1);
+            if (is_numeric($name)) {
+                $users->where('cellphone', 'like', "%$name%");
+            }else {
+                $users->where('nickname', 'like', "%$name%");
+            }
+
+        $users = $users
+                ->leftjoin('orders', 'users.uid', '=', 'orders.student_uid')
+                ->select('users.uid', 'users.nickname', 'users.cellphone', 'users.email', 'users.regdate', 'users.lastlogin', 'users.isactive', DB::raw('count(orders.student_uid) as order_num'))
+                ->groupby('users.uid')
+                ->orderby($field, $order)
+                ->get();
                     //  由前端分页, 此处暂时用不到
                     //  ->paginate(10);
 // dd($users);
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -9,7 +9,7 @@
             <legend>用户查询</legend>
             <div class="form-group">
               {{-- <label for="">用户搜索</label> --}}
-              <input type="text" class="form-control" id="searchName" name="name" placeholder="请输入用户名, 模糊搜索">
+              <input type="text" class="form-control" id="searchName" name="name" placeholder="请输入用户名或手机号">
               <input type="hidden" name="field" value="uid">
               <input type="hidden" name="order" value="asc">
             </div>
