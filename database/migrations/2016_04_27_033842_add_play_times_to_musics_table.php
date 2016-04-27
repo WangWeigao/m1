2ae199bd2257da257f93c316b456147912f6619d
WangWeigao <wangweigao@gmail.com>
2016-04-27 11:40:02 +0800

    在 musics 表中添加 play_times 字段

---
+++ database/migrations/2016_04_27_033842_add_play_times_to_musics_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPlayTimesToMusicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('musics', function (Blueprint $table) {
+            $table->integer('play_times');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('musics', function (Blueprint $table) {
+            $table->dropColumn('play_times');
+        });
+    }
+}
