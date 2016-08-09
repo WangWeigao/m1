7d71a01e457c2fe25e4a41a57303eb2b79da32cc
WangWeigao <wangweigao@gmail.com>
2016-08-09 14:55:01 +0800

    [功能改进] 1. 用户管理 > 弹奏记录 中修改"用户名称"显示, 如果用户没有设置名称, 则显示用户ID

--- resources/views/playRecords.blade.php
+++ resources/views/playRecords.blade.php
@@ -46,7 +46,7 @@
         <tr>
             <th>乐曲名</th>
             <th>用户姓名</th>
-            <th>用户ID</th>
+            {{-- <th>用户ID</th> --}}
             <th>WAV文件路径</th>
             <th>源MIDI文件路径</th>
             <th>匹配后的MIDI文件路径</th>
@@ -56,8 +56,7 @@
         @forelse($play_records as $item)
             <tr>
                 <td>{{ $item->music->name or '-' }}</td>
-                <td><a href="basicinfo/{{ $item->user->uid or '#' }}">{{ $item->user->nickname or '-' }}</a></td>
-                <td><a href="basicinfo/{{ $item->user->uid or '#' }}">{{ $item->user->uid or '用户不存在'}}</a></td>
+                <td><a href="basicinfo/{{ $item->uid or '#' }}">{{ $item->user->nickname or $item->uid }}</a></td>
                 <td><a href="http://120.26.243.208{{ $item->wav_path }}">{{ $item->wav_path }}</a></td>
                 <td>
                     @forelse($item->origin_midi_path as $item_midi)
